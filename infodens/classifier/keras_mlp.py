0049dda9a96c328a531d08e0e06c7c301e0916d8
ahmad-taie <a.gilani@hotmail.com>
2018-05-22 19:19:51 +0200

    added dropout to keras_mlp

--- infodens/classifier/keras_mlp.py
+++ infodens/classifier/keras_mlp.py
@@ -7,7 +7,7 @@ Created on Aug 23, 2016
 from infodens.classifier.classifier import Classifier
 import argparse
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.utils import np_utils
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn import preprocessing
@@ -15,14 +15,17 @@ import numpy
 
 
 # Function to create model, required for KerasClassifier
-def create_model(inputSize=None, layers=[10], n_classes=None, optimizer='adam', init='glorot_uniform'):
+def create_model(inputSize=None, layers=[10], n_classes=None, optimizer='adam', dropoutRate=0.3,
+                 init='glorot_uniform'):
     # create model
 
     def modelCreator():
         model = Sequential()
         model.add(Dense(layers[0], input_dim=inputSize, kernel_initializer=init, activation='relu'))
+        model.add(Dropout(dropoutRate))
         for i in range(1, len(layers)):
             model.add(Dense(layers[i], activation='relu'))
+            model.add(Dropout(dropoutRate))
         model.add(Dense(n_classes, kernel_initializer=init, activation='softmax'))
         # Compile model
         model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
@@ -48,17 +51,21 @@ class Keras_MLP(Classifier):
                             type=int, default=5)
         parser.add_argument("-batch_size", help="Size of mini batch",
                             type=int, default=64)
+        parser.add_argument("-dropout", help="Dropout rate",
+                            type=float, default=0.3)
         return parser.parse_args(self.args)
 
     def train(self):
 
         listOfHL = [int(layer) for layer in self.args.hidden_layers.split(",")]
-        print("Training {0} hidden layer(s) of size(s) {1}.".format(len(listOfHL),listOfHL))
+        print("Training {0} hidden layer(s) of size(s) {1}.".format(len(listOfHL), listOfHL))
+        print("Using dropout with rate: {0}".format(self.args.dropout))
         inputDim = self.Xtrain.get_shape()[1]
 
         n_classes = len(set(self.ytrain))
 
-        clf = KerasClassifier(build_fn=create_model(inputDim, listOfHL, n_classes),
+        clf = KerasClassifier(build_fn=create_model(inputDim, listOfHL, n_classes,
+                                                    dropoutRate=self.args.dropout),
                               epochs=self.args.epochs, batch_size=64, verbose=1)
         clf.fit(self.Xtrain, self.ytrain)
         self.model = clf
